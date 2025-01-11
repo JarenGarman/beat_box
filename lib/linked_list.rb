6f36f30843bde0b10d0e82736005d2b4a782b689
Jaren Garman <jarengarman@protonmail.com>
2025-01-11 15:48:54 -0500

    feat: Add append method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
+require_relative 'node'
+
 # Create a Linked List in Ruby!
 class LinkedList
   attr_reader :head
+
+  def append(data)
+    @head = Node.new(data)
+  end
 end
