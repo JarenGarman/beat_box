db5205333d4631eeb44b7a567e6f543ec09c8e77
Jaren Garman <jarengarman@protonmail.com>
2025-01-11 15:40:47 -0500

    test: Has no head

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe LinkedList do
 
   describe '#initialize' do
     it { is_expected.to be_instance_of described_class }
+
+    it 'has no head' do
+      expect(list.head).to be_nil
+    end
   end
 end
