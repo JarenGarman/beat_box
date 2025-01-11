bc5edcdbffbd3b0637bb4794b99f4a5803217733
Jaren Garman <jarengarman@protonmail.com>
2025-01-11 17:56:23 -0500

    test: Add tests for append

--- spec/beat_box_spec.rb
+++ spec/beat_box_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe BeatBox do
       expect(bb.list).to be_instance_of LinkedList
     end
   end
+
+  describe '#append' do
+    it 'can split input and create multiple nodes' do
+      bb.append('deep doo ditt')
+
+      expect(bb.list.head.next_node.data).to eq('doo')
+    end
+
+    it 'can create single node' do
+      bb.append('deep')
+
+      expect(bb.list.count).to eq(1)
+    end
+  end
 end
