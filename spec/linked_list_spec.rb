dd391277d08f2a8ffac91cd8fc91e2824c832217
Jaren Garman <jarengarman@protonmail.com>
2025-01-11 15:47:31 -0500

    test: Add test for append method

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe LinkedList do
       expect(list.head).to be_nil
     end
   end
+
+  describe '#append' do
+    it 'can can add a node' do
+      list.append('doop')
+
+      expect(list.head).to be_instance_of Node
+    end
+  end
 end
